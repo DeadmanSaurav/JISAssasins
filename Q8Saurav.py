8b29a59535c709d7a4c273adf6744f889692d56c
DeadmanSaurav <46235772+DeadmanSaurav@users.noreply.github.com>
2018-12-29 12:58:04 +0530

    Create Q8Saurav.py

---
+++ Q8Saurav.py
@@ -0,0 +1,9 @@
+x = input("enter the value of x")
+y = input("enterrhe value of y")
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
